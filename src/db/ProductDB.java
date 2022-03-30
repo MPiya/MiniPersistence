cdfa07aeb1bed989bc0e91bec33e819d98d54b34
Zuher1990 <93292950+10407529@users.noreply.github.com>
2022-03-30 23:08:08 +0200

    cdsf

--- src/db/ProductDB.java
+++ src/db/ProductDB.java
@@ -1,16 +1,63 @@
 package db;
-
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import model.Product;
+import model.SaleOrder;
+
+
+
 
 public class ProductDB implements ProductDBIF {
-	private Product product;
+	private PreparedStatement findProductbyid;
 	
+	private static final String find_by_id = "select productID, salesPrice, "
+										+ "countryOfOrigin, currentStock, "
+										+ "productType, purchasePrice, "
+										+ "location_ID, Supplier_ID  where productID=? ";
 	
-	
-	
-	public Product findProductbyid(int id) {
-		Product product= findProductbyid(int id);
-        return product;
+	public Product findProductbyid(int productID)throws SQLException {
+		Product product = null;
+		try {
+			findProductbyid.setInt(1, productID);
+			ResultSet rs = findProductbyid.executeQuery();
+			if (rs.next()) {
+				product = buildObject(rs);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return product;
 	}
+	
+	
+	
+
+	private Product buildObject(ResultSet rs) throws SQLException {
+		Product product = new Product();
+		try {
+			product.setName(rs.getString("product name"));
+			product.setProductID (rs.getInt("Product ID"));
+			product.setPurchasePrice(rs.getDouble("Purchase Price"));
+			product.setCountryOfOrigin(rs.getString("Country Of Origin"));
+			product.setMinStock(rs.getInt("Min Stock"));
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+		}
+		return product;
+	}
+//	public boolean update (SaleOrder order) throws SQLException{
+//	
+//	try {
+//		
+//		}
+//	} catch (SQLException e) {
+//		e.printStackTrace();
+//	}
+//	return product;
+//}
+
 	
 }
--- src/db/ProductDBIF.java
+++ src/db/ProductDBIF.java
@@ -1,7 +1,11 @@
 package db;
+import java.sql.SQLException;
+
 import model.*;
 
 public interface ProductDBIF {
 	
-	public Product findProductbyid(int id);
+	public Product findProductbyid(int id)throws SQLException;
+	
+	public boolean update (SaleOrder order) throws SQLException;
 }
