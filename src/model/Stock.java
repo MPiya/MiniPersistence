e05aea775baa7a8aac8a20f431f8977e3e2a2b45
Zuher1990 <93292950+10407529@users.noreply.github.com>
2022-03-30 11:30:58 +0200

    Clothing

---
+++ src/model/Stock.java
@@ -0,0 +1,56 @@
+package model;
+
+public class Stock {
+	private String locationID;
+	private int stockQuantity;
+	private int minStock;
+	
+	
+	
+	public Stock(String locationID, int stockQuantity, int minStock) {
+		
+		this.locationID = locationID;
+		this.stockQuantity = stockQuantity;
+		this.minStock = minStock;
+	}
+
+
+
+	public String getLocationID() {
+		return locationID;
+	}
+
+
+
+	public void setLocationID(String locationID) {
+		this.locationID = locationID;
+	}
+
+
+
+	public int getStockQuantity() {
+		return stockQuantity;
+	}
+
+
+
+	public void setStockQuantity(int stockQuantity) {
+		this.stockQuantity = stockQuantity;
+	}
+
+
+
+	public int getMinStock() {
+		return minStock;
+	}
+
+
+
+	public void setMinStock(int minStock) {
+		this.minStock = minStock;
+	}
+	
+	
+	
+	
+}
---
+++ src/model/Supplier.java
@@ -0,0 +1,82 @@
+package model;
+
+public class Supplier {
+	private String name;
+	private String address;
+	private String country;
+	private String phoneNo;
+	private String email;
+	private int supplierID;
+	
+	
+	public Supplier(String name, String address, String country, String phoneNo, String email,int supplierID ) {
+		this.name= name;
+		this.address= address;
+		this.country=country;
+		this.phoneNo= phoneNo;
+		this.email= email;
+		this.supplierID= supplierID;
+		
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public String getCountry() {
+		return country;
+	}
+
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+
+
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public int getSupplierID() {
+		return supplierID;
+	}
+
+
+	public void setSupplierID(int supplierID) {
+		this.supplierID = supplierID;
+	}
+
+}
