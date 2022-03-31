3719fbfa4a3eb50c9e4ba3f479fc4f5793f9ccb5
Bon <masterpiyaboot@hotmail.com>
2022-03-31 09:31:09 +0200

    Add CRUD

--- src/db/CustomerDB.java
+++ src/db/CustomerDB.java
@@ -3,6 +3,7 @@ package db;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,8 +15,11 @@ public class CustomerDB implements CustomerDBIF {
 		
 		private static final String findByPhone = "select fname,lname,zipcode,city,phoneno,cid,isClupType,address from customer where phoneno = ? ";
 		private static final String showCustomer = " select fname  from customer where cid =1";
+		private static final String createCustomer = "INSERT INTO customer (fname, lname,zipcode,city,phoneno,isClupType,address)	 "
+				+ "values (?, ?, ?,?,?,?,?)";
+		private static final String deleteCustomer = " Delete from customer where cid = ? ";
+		private PreparedStatement createCus, findCBP, showCus, deleteCus;
 		
-		private PreparedStatement findAll, findById, update, findCBP, showCus;
 
 		public CustomerDB () throws DataAccessException {
 			try {
@@ -24,6 +28,11 @@ public class CustomerDB implements CustomerDBIF {
 						.prepareStatement(findByPhone);
 				showCus = DBConnection.getInstance().getConnection()
 						.prepareStatement(showCustomer);
+				createCus = DBConnection.getInstance().getConnection()
+						.prepareStatement(createCustomer);
+				deleteCus =  DBConnection.getInstance().getConnection()
+						.prepareStatement(deleteCustomer);
+				
 			} catch (SQLException e) {
 			 System.out.println("ERROR");
 			}
@@ -67,7 +76,7 @@ public class CustomerDB implements CustomerDBIF {
 				
 		}
 		
-		private List<Customer> buildCusObjects(ResultSet rs) throws SQLException {
+		 public List<Customer> buildCusObjects(ResultSet rs) throws SQLException {
 			List<Customer> res = new ArrayList<>();
 			while(rs.next()) {
 				res.add(buildCusObject(rs));
@@ -86,16 +95,32 @@ public class CustomerDB implements CustomerDBIF {
 			         e.printStackTrace();
 			      } 
 			}
+		public void insertCustomertoDB(String fname, String lname, String zipcode, String city, String phoneno, Boolean isClupType, String Address) throws SQLException {
+			createCus.setString(1,fname);
+			createCus.setString(2,lname);
+			createCus.setString(3,zipcode);
+			createCus.setString(4,city);
+			createCus.setString(5,phoneno);
+			createCus.setBoolean(6, false);
+			createCus.setString(7, Address);
+			// use executeUpdate instead of executequery so it wont give the error resultset doesnt return.
+			createCus.executeUpdate();
 		
-	
-//work fine will be changed to comment later
-		 public static void main(String[] args) throws DataAccessException {
-			CustomerDB c = new CustomerDB();
-			
+			}
 		
-			c.findCustomerByPhone("22222");
-			c.showMe();
+		public void deleteCustomerbyID(int cid)throws SQLException {
+			deleteCus.setInt(1, cid);
+			deleteCus.executeUpdate();
 			
 		}
+		
+public static void main(String[] args) throws SQLException,DataAccessException {
+	CustomerDB c = new CustomerDB();
 	
+	//		c.findCustomerByPhone("22222");
+	c.showMe();
+	c.deleteCustomerbyID(11);
 }
+}
+
+
--- src/db/CustomerDBIF.java
+++ src/db/CustomerDBIF.java
@@ -1,13 +1,16 @@
 package db;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import model.Customer;
 
+
 public interface CustomerDBIF {
 
-	public Customer findCustomerByPhone(String phoneNo) throws DataAccessException ;
-	
+	 Customer findCustomerByPhone(String phoneNo) throws DataAccessException ;
+	 List<Customer> buildCusObjects(ResultSet rs) throws SQLException;
 }
 
 
