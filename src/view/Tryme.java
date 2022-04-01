74b87a33fd39307bfc59fa6ecb93bea15b09536c
Women Respecter <93257462+FreddyFlamingo@users.noreply.github.com>
2022-04-01 12:17:02 +0200

    Update Tryme.java

--- src/view/Tryme.java
+++ src/view/Tryme.java
@@ -1,10 +1,16 @@
 package view;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 import controller.CustomerCtr;
 import controller.ProductCtr;
+import controller.SaleOrderController;
 import db.DataAccessException;
+import model.Customer;
+import model.Product;
+import model.SaleOrder;
+import model.SalesOrderLine;
 
 public class Tryme {
 
@@ -14,8 +20,9 @@ public class Tryme {
 	}
 	
 	public static void main(String[] args)  throws DataAccessException, SQLException {
-		CustomerCtr b = new CustomerCtr();
+		CustomerCtr c = new CustomerCtr();
 		ProductCtr p = new ProductCtr();
+		SaleOrderController o = new SaleOrderController();
 		/* work fine
 		b.deleteCustomerById(9);
 		*/
@@ -27,6 +34,27 @@ public class Tryme {
 		/* Work fine
 		p.findProductById(6); 
 		*/
+		String date = "2022-08-16";
+		String dDate = "2022-12-30";
+        
+        LocalDate sDate = LocalDate.parse(date);
+        LocalDate eDate = LocalDate.parse(dDate);
+		
+		Customer cus = c.findCustomerByPhone("004511223344");
+		SaleOrder so = o.createSalesOrderAddCustomer(sDate, "Ikke sendt", eDate, 0.0, cus, 2400);
+		
+		Product pr = p.findProductById(6);
+		Product pr2 = p.findProductById(5);
+		
+		SalesOrderLine sol = o.createSalesOrderLineAddProduct(2, 1000, pr);
+		SalesOrderLine sol2 = o.createSalesOrderLineAddProduct(2, 1000, pr2);
+		
+		so.addSalesOrderLine(sol);
+		so.addSalesOrderLine(sol2);
+		
+		o.saveOrder(so);
+		
+				
 	}
 	
 }
